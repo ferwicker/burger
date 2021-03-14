b63ffaf0f79e178d660e7c8f4a75ed276f6347a6
Fer Wicker <fer.wicker@gmail.com>
2021-03-14 14:44:49 +1100

    add server.js content

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 // Dependencies
 const express = require('express');
+const exphbs = require('express-handlebars');
+
+const PORT = process.env.PORT || 3000;
+const app = express();  
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+// Set Handlebars.
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+const routes = require('./controllers/burger_controller');
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`App now listening at localhost:${PORT}`));
