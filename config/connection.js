e12fa5fcfe0f0fd184ef4b511505508258039b5a
Fer Wicker <fer.wicker@gmail.com>
2021-03-14 20:12:27 +1100

    setting up JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,20 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+let connection; 
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'yourRootPassword',
     database: 'burgers_db',
   });
-  
-  connection.connect((err) => {
-    if (err) {
-      console.error(`error connecting: ${err.stack}`);
-      return;
-    }
-    console.log(`connected as id ${connection.threadId}`);
-  });
+};
+
+ 
+  connection.connect();
   
   module.exports = connection;
