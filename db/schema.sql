e2f0858518b935d06d4ea5cb1af694686e9893cf
Fer Wicker <fer.wicker@gmail.com>
2021-03-14 11:33:31 +1100

    db directory and sql schema and seeds files

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO burgers (burger_name) VALUES ('Royale With Cheese'), ('Chook Norris'), ('Triple Double');
